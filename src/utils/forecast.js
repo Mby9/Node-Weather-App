2eba11d8fb203210121de99737298f8ebf029820
Mby9 <jsmayankyadav@gmail.com>
2019-10-27 16:14:26 +0530

    Add high and low temp

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/8c262d1bdbaed79e9900e31885a12e7c/' + latitude + ',' + longitude
+    const url = 'https://api.darksky.net/forecast/8c262d1bdbaed79e9900e31885a12e7c/' + latitude + ',' + longitude + '?unit=si'
     
     request({ url, json: true }, (error, { body }) => {
         if (error) {
@@ -9,7 +9,12 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary 
+                + ' It is currently ' + body.currently.temperature 
+                + ' degress out. There is a ' + body.currently.precipProbability 
+                + '% chance of rain. \n'
+                + 'Temperature maximum is: ' + body.daily.data[0].temperatureHigh + " degrees Celcius.\n"
+                + 'Temperature minimum is: ' + body.daily.data[0].temperatureLow + " degrees Celcius.")
         }
     })
 }
